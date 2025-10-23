c82d020f699a89689f8cd26f601f23df9e874a79
oonoren <dayelian9@gmail.com>
2025-10-23 19:11:04 +0900

    テストユーザーログインボタンの実装

--- db/seeds/development/users.rb
+++ db/seeds/development/users.rb
@@ -1,7 +1,16 @@
+# テストアカウント（フロントエンドのテストログイン用）
+test_user = User.find_or_initialize_by(email: "test@example.com", activated: true)
+if test_user.new_record?
+  test_user.name = "テストユーザー"
+  test_user.password = "password"
+  test_user.save!
+  puts "Created test user: test@example.com / password"
+end
+
 10.times do |n|
   name = "user#{n}"
   email = "#{name}@example.com"
-  # find_by(email: email, activated: true) 
+  # find_by(email: email, activated: true)
   # オブジェクトが存在する => 取得
   # オブジェクトが存在しない => 新規作成
   user = User.find_or_initialize_by(email: email, activated: true)
