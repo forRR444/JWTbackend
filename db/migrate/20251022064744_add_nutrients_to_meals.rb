27163338d5138e207f980d67fa04d86ac03ba89c
oonoren <dayelian9@gmail.com>
2025-11-01 10:38:32 +0900

    Mealsテーブルに栄養情報カラムを追加

---
+++ db/migrate/20251022064744_add_nutrients_to_meals.rb
@@ -0,0 +1,7 @@
+class AddNutrientsToMeals < ActiveRecord::Migration[8.0]
+  def change
+    add_column :meals, :protein, :decimal, precision: 8, scale: 1, comment: "たんぱく質 (g)"
+    add_column :meals, :fat, :decimal, precision: 8, scale: 1, comment: "脂質 (g)"
+    add_column :meals, :carbohydrate, :decimal, precision: 8, scale: 1, comment: "炭水化物 (g)"
+  end
+end
