02ebdd0b730158dcc3d1a9fef858ac017cdee506
oonoren <dayelian9@gmail.com>
2025-11-01 11:11:29 +0900

    MealTagsテーブルの追加

---
+++ db/migrate/20251026000002_create_meal_tags.rb
@@ -0,0 +1,11 @@
+class CreateMealTags < ActiveRecord::Migration[8.0]
+  def change
+    create_table :meal_tags do |t|
+      t.references :meal, null: false, foreign_key: true
+      t.references :tag, null: false, foreign_key: true
+      t.timestamps
+    end
+
+    add_index :meal_tags, [:meal_id, :tag_id], unique: true
+  end
+end
