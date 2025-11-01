08c3e97f2ba71c7970ebfdf09e1075d03c17895b
oonoren <dayelian9@gmail.com>
2025-11-01 10:39:29 +0900

    Usersテーブルに栄養目標カラムを追加

---
+++ db/migrate/20251022101809_add_nutrition_goals_to_users.rb
@@ -0,0 +1,8 @@
+class AddNutritionGoalsToUsers < ActiveRecord::Migration[8.0]
+  def change
+    add_column :users, :target_calories, :integer, comment: "目標カロリー (kcal)"
+    add_column :users, :target_protein, :decimal, precision: 8, scale: 1, comment: "目標たんぱく質 (g)"
+    add_column :users, :target_fat, :decimal, precision: 8, scale: 1, comment: "目標脂質 (g)"
+    add_column :users, :target_carbohydrate, :decimal, precision: 8, scale: 1, comment: "目標炭水化物 (g)"
+  end
+end
