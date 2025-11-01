61e23495a8541cc8d543f99271de4e40dbd8df1b
oonoren <dayelian9@gmail.com>
2025-11-01 10:37:40 +0900

    Foodsテーブルの追加

---
+++ db/migrate/20251022062326_create_foods.rb
@@ -0,0 +1,18 @@
+class CreateFoods < ActiveRecord::Migration[8.0]
+  def change
+    create_table :foods do |t|
+      t.string :food_code, null: false, comment: "食品番号 (例: 01)"
+      t.string :index_number, null: false, comment: "索引番号 (例: 01001)"
+      t.string :name, null: false, comment: "食品名"
+      t.decimal :calories, precision: 8, scale: 1, comment: "エネルギー (kcal/100g)"
+      t.decimal :protein, precision: 8, scale: 1, comment: "たんぱく質 (g/100g)"
+      t.decimal :fat, precision: 8, scale: 1, comment: "脂質 (g/100g)"
+      t.decimal :carbohydrate, precision: 8, scale: 1, comment: "炭水化物 (g/100g)"
+
+      t.timestamps
+    end
+
+    add_index :foods, :name
+    add_index :foods, :index_number, unique: true
+  end
+end
