d4211031e2be97233093d4732faac95ec307719d
oonoren <dayelian9@gmail.com>
2025-11-01 11:11:33 +0900

    MealTagモデルの追加

---
+++ app/models/meal_tag.rb
@@ -0,0 +1,8 @@
+class MealTag < ApplicationRecord
+  # リレーション
+  belongs_to :meal
+  belongs_to :tag
+
+  # バリデーション
+  validates :meal_id, uniqueness: { scope: :tag_id, message: "このタグは既に追加されています" }
+end
