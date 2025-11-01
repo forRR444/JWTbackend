6e1234cc7346fcbfc2d141a77e22d82b8b02ee80
oonoren <dayelian9@gmail.com>
2025-11-01 11:11:02 +0900

    Tagsテーブルの追加

---
+++ db/migrate/20251026000001_create_tags.rb
@@ -0,0 +1,10 @@
+class CreateTags < ActiveRecord::Migration[8.0]
+  def change
+    create_table :tags do |t|
+      t.string :name, null: false, comment: "タグ名（例: 外食、自炊）"
+      t.timestamps
+    end
+
+    add_index :tags, :name, unique: true
+  end
+end
