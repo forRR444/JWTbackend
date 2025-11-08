a6a7d814a76efa5e6eae8305a5fadbd08854e363
oonoren <dayelian9@gmail.com>
2025-11-08 17:03:53 +0900

    RuboCop違反を修正

--- config/initializers/user_auth.rb
+++ config/initializers/user_auth.rb
@@ -32,7 +32,7 @@ module UserAuth
 
   # 署名・検証に使用する秘密鍵
   mattr_accessor :token_secret_signature_key
-  self.token_secret_signature_key = ENV['JWT_SECRET_KEY'] || Rails.application.credentials.secret_key_base
+  self.token_secret_signature_key = ENV["JWT_SECRET_KEY"] || Rails.application.credentials.secret_key_base
 
   # 署名・検証に使用する公開鍵(RS256)
   mattr_accessor :token_public_key
